cc3c332a1d72871d6a660f3cadea7c4fc25f2771
Okuhle <okuhlen.20@gmail.com>
2023-03-30 15:09:54 +0000

    binary trees 0-6

---
+++ binary_trees.h
@@ -0,0 +1,25 @@
+#ifndef _BINARY_TREES_H_
+#define _BINARY_TREES_H_
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+
+typedef struct binary_tree_s {
+    int n;
+    struct binary_tree_s *parent;
+    struct binary_tree_s *left;
+    struct binary_tree_s *right;
+} binary_tree_t;
+
+void binary_tree_print(const binary_tree_t *);
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+void binary_tree_delete(binary_tree_t *tree);
+int binary_tree_is_leaf(const binary_tree_t *node);
+int binary_tree_is_root(const binary_tree_t *node);
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
+
+#endif
