649fac56fcb9c2cfbcef5863cda526dd7f1c212e
Okuhle <okuhlen.20@gmail.com>
2023-03-31 00:03:16 +0000

    modded 1-2

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -20,14 +20,13 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	if (new_node == NULL)
 		return (NULL);
 
-	new_node->n = value;
-	new_node->parent = parent;
-	new_node->left = NULL;
-	new_node->right = parent->right;
 	if (parent->right != NULL)
+	{
+		new_node->right = parent->right;
 		parent->right->parent = new_node;
-
+	}
 	parent->right = new_node;
+
 	return (new_node);
 }
 
