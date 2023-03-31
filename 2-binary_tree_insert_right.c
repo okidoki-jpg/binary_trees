82e16d7332783842de6ebd73eec0860213ff252e
Okuhle <okuhlen.20@gmail.com>
2023-03-31 14:25:11 +0000

    modded 1, 2 & 9

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -12,10 +12,12 @@
 
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
+	binary_tree_t *new_node;
+
 	if (parent == NULL)
 		return (NULL);
 
-	binary_tree_t *new_node = binary_tree_node(parent, value);
+	new_node = binary_tree_node(parent, value);
 
 	if (new_node == NULL)
 		return (NULL);
--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,16 +9,18 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height, right_height;
+	size_t left_height = 0, right_height = 0;
 
-	if (tree == NULL)
+	if (!tree)
 		return (0);
 
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
+	if (tree->left)
+		left_height += 1 + binary_tree_height(tree->left);
+	if (tree->right)
+		right_height += 1 + binary_tree_height(tree->right);
 
 	if (left_height > right_height)
-		return (left_height + 1);
+		return (left_height);
 	else
-		return (right_height + 1);
+		return (right_height);
 }
